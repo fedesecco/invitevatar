50f4f333f1a6f2e8243c57ee7e0ce29c8f718ec2
Federico Secco <federico.secco7@gmail.com>
2025-12-03 20:41:26 +0100

    Generated hero page

---
+++ apps/invitevatar/src/app/views/landing/landing.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  imports: [],
+  templateUrl: './landing.html',
+  styleUrl: './landing.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Landing {}
