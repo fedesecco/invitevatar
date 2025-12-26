85104c057394d6970b04f78bbfdbd67c57d7d290
Federico Secco <federico.secco7@gmail.com>
2025-12-26 11:41:43 +0100

    Added transloco

--- apps/invitevatar/src/app/app.config.ts
+++ apps/invitevatar/src/app/app.config.ts
@@ -1,17 +1,29 @@
 import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
-  provideZonelessChangeDetection,
+  provideZonelessChangeDetection, isDevMode,
 } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { TranslocoHttpLoader } from './transloco-loader';
+import { provideTransloco } from '@jsverse/transloco';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideAnimations(),
-    provideRouter(appRoutes),
+    provideRouter(appRoutes), provideHttpClient(), provideTransloco({
+        config: { 
+          availableLangs: ['en', 'it'],
+          defaultLang: 'en',
+          // Remove this option if your application doesn't support changing language in runtime.
+          reRenderOnLangChange: true,
+          prodMode: !isDevMode(),
+        },
+        loader: TranslocoHttpLoader
+      }),
   ],
 };
